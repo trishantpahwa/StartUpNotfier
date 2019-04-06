4ff50309b168bbc2bba2d7bc934646bb5630daf3
Trishant Pahwa <trishantpahwa@gmail.com>
2019-04-06 23:00:25 +0530

    Added file_write function

--- install_linux.py
+++ install_linux.py
@@ -27,3 +27,19 @@ def install():
     except IOError:
         print 'File not found'
 
+def file_write():
+    file_location = '\\usr\\share\\StartUpNotifier\\'
+    file_name = file_location + 'start_up_log.txt'
+    try:
+		with open(file_name, 'w') as file:
+			phone_number = str(raw_input('Enter phone number: '))
+			api_key = str(raw_input('Enter API-Key: '))
+			api_secret = str(raw_input('Enter API-Secret: '))
+			data = { 'Phone Number': phone_number, 'API-Key': api_key, 'API-Secret': api_secret }
+			file_data = json.dumps(data)
+			file.write(file_data)
+			return data
+    except IOError, TypeError:
+		print IOError
+		print TypeError
+
