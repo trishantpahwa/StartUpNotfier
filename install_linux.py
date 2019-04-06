7216766d6c3dba8294f68867c05bc6b89186d51b
Trishant Pahwa <trishantpahwa@gmail.com>
2019-04-06 22:59:59 +0530

    Added install function.

--- install_linux.py
+++ install_linux.py
@@ -2,7 +2,7 @@ import os
 import json
 
 
-def get_startup_location(username):
+def get_startup_location():
     start_up_sh = 'start_up.sh'
     start_up_location = '\\etc\\init.d\\' + start_up_sh
     try:
@@ -12,4 +12,18 @@ def get_startup_location(username):
                 start_up_file.write(start_up_data)
     except IOError:
         print 'File not found.'
+
+def install():
+    start_up = 'startup.py'
+    start_up_script = '\\usr\\share\\'
+    if 'StartUpNotifier' not in os.listdir(start_up_script):
+        os.mkdir(start_up_script + '\\StartUpNotifier')
+    start_up_script += '\\StartUpNotifier\\' + start_up
+    try:
+        with open(start_up_script, 'w') as start_up_file:
+            with open(start_up, 'r') as start_up:
+                start_up_data = start_up.read()
+                start_up_file.write(start_up_data)
+    except IOError:
+        print 'File not found'
 
