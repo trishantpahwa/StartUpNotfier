4c155bdfa796bc618349ef7979b9e7c0ed96bed8
Trishant Pahwa <trishantpahwa@gmail.com>
2019-04-06 22:47:46 +0530

    Added function to write file containing basic info on install

--- install_windows.py
+++ install_windows.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 def get_startup_location(username):
@@ -26,8 +27,25 @@ def install(username):
     except IOError:
         print 'File not found'
 
+def file_write(username):
+    file_location = 'C:\\Users\\' + username + '\\StartUpNotifier\\'
+    file_name = file_location + 'start_up_log.txt'
+    try:
+		with open(file_name, 'w') as file:
+			phone_number = str(raw_input('Enter phone number: '))
+			api_key = str(raw_input('Enter API-Key: '))
+			api_secret = str(raw_input('Enter API-Secret: '))
+			data = { 'Phone Number': phone_number, 'API-Key': api_key, 'API-Secret': api_secret }
+			file_data = json.dumps(data)
+			file.write(file_data)
+			return data
+    except IOError, TypeError:
+		print IOError
+		print TypeError
+
 def install_StartUpNotifier():
     install('HP')
+    file_write('HP')
     get_startup_location('HP')
 
 install_StartUpNotifier()
